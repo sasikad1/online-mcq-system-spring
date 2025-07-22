71b70f6241d51b5be11c12193268b5ad23d1a783
sasikad1 <sasikadilum40@gmail.com>
2025-07-22 11:38:34 +0530

    initial commit

---
+++ src/main/java/com/sasika/mcq/OnlineMcqSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sasika.mcq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMcqSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMcqSystemApplication.class, args);
+	}
+
+}
